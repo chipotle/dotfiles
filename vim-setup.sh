0c09bad05c7a6e2d8271d198df6600a5b2a3412a
Watts Martin <layotl@gmail.com>
2019-08-18 09:51:40 -0700

    Vim setup improvements

--- vim-setup.sh
+++ vim-setup.sh
@@ -5,7 +5,6 @@ mkdir -p ~/.vim/pack/bundle/start
 
 pushd ~/.vim/pack/bundle/opt
 git clone https://github.com/lifepillar/vim-solarized8
-git clone https://github.com/tpope/vim-rails
 git clone https://github.com/rakr/vim-one
 git clone https://github.com/jnurmine/zenburn
 git clone https://github.com/morhetz/gruvbox 
@@ -15,8 +14,11 @@ git clone https://github.com/ctrlpvim/ctrlp.vim
 git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/tpope/vim-surround
 git clone https://github.com/tpope/vim-endwise
+git clone https://github.com/tpope/vim-rails
 git clone https://github.com/editorconfig/editorconfig-vim
 git clone https://github.com/rizzatti/dash.vim
 
 popd
+
+cp update.sh ~/.vim
 
