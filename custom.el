574ffa8b2d57bc64378b85d312898068bb8f1084
Watts Martin <layotl@gmail.com>
2025-05-23 20:14:08 -0400

    Add emacs init files

---
+++ custom.el
@@ -0,0 +1,27 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(apropos-sort-by-scores t)
+ '(custom-enabled-themes '(modus-operandi-tinted))
+ '(markdown-asymmetric-header t)
+ '(modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))
+ '(modus-themes-variable-pitch-ui t)
+ '(package-selected-packages
+   '(cape corfu eldoc-box embark-consult magit marginalia markdown-mode
+          mood-line orderless php-mode vertico web-mode yaml-mode))
+ '(ring-bell-function 'ignore)
+ '(sentence-end-double-space nil)
+ '(tool-bar-mode nil)
+ '(visible-bell t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:weight regular :height 130 :width normal :foundry "nil" :family "MonoLisa"))))
+ '(eldoc-box-body ((t (:background "#efe9dd" :foreground "#000000" :family "SF Pro"))))
+ '(tab-line ((t (:inherit modus-themes-ui-variable-pitch :background "#e0d4ce"))))
+ '(variable-pitch ((t (:height 130 :family "SF Pro")))))
---
+++ init.el
@@ -0,0 +1,353 @@
+;; use separate file for auto-generated customizations
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (and custom-file
+	   (file-exists-p custom-file))
+  (load custom-file nil :nomessage))
+
+;; === Keybindings ===
+
+(keymap-global-set "<remap> <list-buffers>" #'ibuffer-list-buffers)
+(global-set-key [remap dabbrev-expand] 'hippie-expand)
+
+;; === Defaults Stuff (mostly boosted from Crafted Emacs) ===
+
+;; revert buffers when underlying file/data changes
+(global-auto-revert-mode 1)
+(customize-set-variable 'global-auto-revert-non-file-buffers t)
+
+;; make Dired guess default target directory when two directories are open
+(customize-set-variable 'dired-dwim-target t)
+
+;; automatically update dired buffers on revisiting their directory
+(customize-set-variable 'dired-auto-revert-buffer t)
+
+;; scroll eshell buffer to bottom on input, only in "this" window
+(customize-set-variable 'eshell-scroll-to-bottom-on-input 'this)
+
+;; pop up dedicated buffers in a different window.
+(customize-set-variable 'switch-to-buffer-in-dedicated-window 'pop)
+
+;; treat manual buffer switching (C-x b for example) the same as
+;; programmatic buffer switching.
+(customize-set-variable 'switch-to-buffer-obey-display-actions t)
+
+;; the number of hours before a buffer is considered "old" by
+;; ibuffer.
+(customize-set-variable 'ibuffer-old-time 24)
+
+;; Typed text replaces the selection if the selection is active,
+;; pressing delete or backspace deletes the selection.
+(delete-selection-mode)
+
+;; Use spaces instead of tabs
+(setq-default indent-tabs-mode nil)
+
+;; Do not save duplicates in kill-ring
+(customize-set-variable 'kill-do-not-save-duplicates t)
+
+;; Better support for files with long lines
+(setq-default bidi-paragraph-direction 'left-to-right)
+(setq-default bidi-inhibit-bpa t)
+(global-so-long-mode 1)
+(global-visual-line-mode t)
+(column-number-mode t)
+
+;; set up dictionary server
+(keymap-set global-map "M-#" #'dictionary-lookup-definition)
+(setq dictionary-server "dict.org")
+
+;; turn on spell checking, if available.
+(with-eval-after-load 'ispell
+  (when (executable-find ispell-program-name)
+    (add-hook 'text-mode-hook #'flyspell-mode)
+    (add-hook 'prog-mode-hook #'flyspell-prog-mode)))
+
+;; Turn on recentf mode
+(add-hook 'after-init-hook #'recentf-mode)
+
+;; save your place in files
+(save-place-mode 1)
+
+;; Enable savehist-mode for command history
+(savehist-mode 1)
+
+;; save the bookmarks file every time a bookmark is made or deleted
+;; rather than waiting for Emacs to be killed.  Useful especially when
+;; Emacs is a long running process.
+(customize-set-variable 'bookmark-save-flag 1)
+
+;; Make scrolling less stuttered
+(setq auto-window-vscroll nil)
+(customize-set-variable 'fast-but-imprecise-scrolling t)
+(customize-set-variable 'scroll-conservatively 101)
+(customize-set-variable 'scroll-margin 1)
+(customize-set-variable 'scroll-preserve-screen-position t)
+
+;; open man pages in their own window, and switch to that window to
+;; facilitate reading and closing the man page.
+(customize-set-variable 'Man-notify-method 'aggressive)
+
+;; keep the Ediff control panel in the same frame
+(customize-set-variable 'ediff-window-setup-function
+                        'ediff-setup-windows-plain)
+
+;; Window configuration for special windows
+(add-to-list 'display-buffer-alist
+             '("\\*Help\\*"
+               (display-buffer-reuse-window display-buffer-pop-up-window)))
+
+(add-to-list 'display-buffer-alist
+             '("\\*Completions\\*"
+               (display-buffer-reuse-window display-buffer-pop-up-window)
+               (inhibit-same-window . t)
+               (window-height . 10)))
+
+;; prefer the newest version of a file
+(customize-set-variable 'load-prefer-newer t)
+
+;; make scripts executable on save
+(add-hook 'after-save-hook
+	  #'executable-make-buffer-file-executable-if-script-p)
+
+;; set initial window size
+(setq initial-frame-alist
+      (append initial-frame-alist
+              '((width . 120)
+                (height . 40))))
+
+;; line numbers
+(global-display-line-numbers-mode)
+(setq-default
+ display-line-numbers-grow-only t
+ display-line-numbers-width 2)
+
+;; === Package Stuff ===
+
+;; set up package archives
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+	("elpa" . "https://elpa.gnu.org/packages/")
+	("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+
+;; always ensure packages are installed
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+;; setup vertico and friends
+(use-package vertico
+  :custom
+  (vertico-cycle t)
+  :init
+  (vertico-mode))
+
+;; (use-package savehist
+;;   :init
+;;   (savehist-mode))
+
+(use-package marginalia
+  :after vertico
+  :config
+  (marginalia-mode))
+
+;; embark: "Emacs Mini-Buffer Actions Rooted in Keymaps"
+(use-package embark
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command))
+
+(use-package embark-consult
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+;; consult: improve a whole bunch of commands with completing-read
+(use-package consult
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-i" . consult-imenu)
+         ("M-I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Tweak the register preview for `consult-register-load',
+  ;; `consult-register-store' and the built-in commands.  This improves the
+  ;; register formatting, adds thin separator lines, register sorting and hides
+  ;; the window mode line.
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep consult-man
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+  )
+
+;; Corfu (completion in region function: in-buffer completion popup)
+(use-package corfu
+  :custom
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-prefix 2)
+  :init
+  (global-corfu-mode)
+  (corfu-popupinfo-mode)
+  (corfu-history-mode))
+
+;; orderless (fuzzy completion matching)
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+;; Cape (completion at point extensions)
+(use-package cape
+  :bind ("C-c p" . cape-prefix-map) ;; press C-c p ? for help
+  :init
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; silence "pcomplete capf" (so says crafted emacs)
+  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify))
+
+;; web mode
+(use-package web-mode
+  :mode
+  (("\\.html\\'" . web-mode)
+   ("\\.phtml\\'" . web-mode)
+   ("\\.tpl\\'" . web-mode)
+   ("\\.erb\\'" . web-mode)
+   ("\\.mustache\\'" . web-mode)))
+
+(setq web-mode-engines-alist
+      '(("django" . "\\.html\\'")) ;; for Tera templates
+      )
+
+;; php mode
+(use-package php-mode
+  :custom
+  (php-mode-coding-style 'psr2))
+
+;; eglot
+(use-package eglot
+  :hook (prog-mode . eglot-ensure)
+  :config
+  (add-to-list 'eglot-server-programs
+               `(php-mode . ,(eglot-alternatives
+                              '(("intelephense" "--stdio"))))))
+(setq eglot-autoshutdown t)
+
+;; markdown
+(use-package markdown-mode
+  :custom
+  (markdown-italic-underscore t)
+  (markdown-open-command "/usr/local/bin/mark"))
+
+;; yaml
+(use-package yaml-mode
+  :mode
+  ("\\.yml\\'" . yaml-mode))
+
+;; eldoc-box (prettier documentation popups)
+(use-package eldoc-box
+  :hook (eglot-managed-mode-hook #'eldoc-box-hover-mode)
+  :bind (("M-?" . eldoc-box-help-at-point)
+         :map eglot-mode-map
+         ("M-?" . eldoc-box-eglot-help-at-point)))
+
+;; mood-line (better modeline)
+(use-package mood-line
+  :config
+  (mood-line-mode)
+  :custom
+  (mood-line-glyph-alist mood-line-glyphs-unicode))
+
+;; editorconfig
+(add-hook 'prog-mode-hook #'editorconfig-mode)
+
+;; magit
+(use-package magit)
+
+;; TODO see how Crafted Emacs adds recent files to startup screen
+;; investigate Treemacs
+;; investigate treesitter modes (Crafted Emacs again?)
