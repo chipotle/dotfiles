1d1bd2e33e22c7e6745c7314efa187c23b4cd0d1
Watts <watts@coyotetracks.org>
2025-10-28 22:22:54 -0400

    Create Linux-specific init/custom files

---
+++ .emacs.d/custom-linux.el
@@ -0,0 +1,16 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 130 :family "IBM Plex Mono"))))
+ '(ef-themes-fixed-pitch ((t (:family "IBM Plex Mono" :height 0.9))) t)
+ '(eldoc-box-body ((t (:family "Noto Sans"))))
+ '(variable-pitch ((t (:height 100 :family "Noto Sans")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
